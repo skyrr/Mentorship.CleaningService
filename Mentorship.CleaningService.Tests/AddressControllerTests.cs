c0a6356d8801d8247f80f2facf51ab3aa1fca889
Volodymyr Neborachko <vneborachkomodest@gmail.com>
2019-12-10 20:27:27 +0200

    Test GetAll() implementation

--- Mentorship.CleaningService.Tests/AddressControllerTests.cs
+++ Mentorship.CleaningService.Tests/AddressControllerTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Mentorship.CleaningService.Models;
 using Mentorship.CleaningService.Repository;
 using Mentorship.CleaningService.WebApi.Controllers;
@@ -22,26 +24,57 @@ namespace Mentorship.CleaningService.Tests
             _serviceProvider = serviceCollection.BuildServiceProvider();
         }
 
-        [SetUp]
-        public void Init()
+        //[SetUp]
+        //public void Init()
+        //{
+        //}
+
+        [Test]
+        public void GetAddress()
         {
             var mock = new Mock<IRepository<Address>>();
             mock.Setup(repo => repo.GetById(It.IsAny<int>())).Returns(GetByIdTest(1));
             var factoryMock = new Mock<IRepositoryFactory>();
             factoryMock.Setup(f => f.GetRepository<Address>()).Returns(mock.Object);
             _addressController = new AddressController(factoryMock.Object);
+
+            var json = _addressController.Get(1);
+            var address = json.Value as Address;
+            Assert.NotNull(json);
+            Assert.NotNull(address);
+            Assert.AreEqual(address.Id, 1);
+            Assert.AreEqual(address.City, "Lviv");
+        }
+        [Test]
+        public void GetAllAddresses()
+        {
+            var mock = new Mock<IRepository<Address>>();
+            mock.Setup(repo => repo.GetAll()).Returns(GetAllTest());
+            var factoryMock = new Mock<IRepositoryFactory>();
+            factoryMock.Setup(f => f.GetRepository<Address>()).Returns(mock.Object);
+            _addressController = new AddressController(factoryMock.Object);
+
+            var json = _addressController.GetAll();
+            var address = json.Value as List<Address>;
+            foreach (var item in address)
+            {
+                Assert.NotNull(json);
+                Assert.NotNull(address);
+                Assert.AreEqual(item.Id, 1);
+                Assert.AreEqual(item.City, "Lviv");
+            }
+
         }
 
-        [Test]
-        public void GetAddress()
+        private IQueryable<Address> GetAllTest()
         {
-            //var json =_addressController.Get();
-            //var address = json.Value as Address;
-            //Assert.NotNull(json);
-            //Assert.NotNull(address);
-            //Assert.AreEqual(address.Id, 1);
-            //Assert.AreEqual(address.City, "Lviv");
-            }
+            var list = new List<Address>();
+            Address a1 = new Address { Id = 1, City = "Lviv" };
+            Address a2 = new Address { Id = 2, City = "Lviv" };
+            list.Add(a1);
+            list.Add(a2);
+            return list.AsQueryable();
+        }
 
         public Address GetByIdTest(int i)
         {
